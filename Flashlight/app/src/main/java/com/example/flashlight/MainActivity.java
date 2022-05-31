19c8105adfdc70db7b0dab2dbbb97d710ceee21d
Md Shayemur Rahman <sayemurrahman99@gmail.com>
2022-05-31 21:23:44 +0300

    acceleration functionality

--- Flashlight/app/src/main/java/com/example/flashlight/MainActivity.java
+++ Flashlight/app/src/main/java/com/example/flashlight/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.flashlight;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,58 +11,77 @@ import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private boolean torchOn = true;
+    private boolean torchon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void setTorchOnOff(View view) {
 
-        // Connect the camera manager.
+    public void onoff(View view){
+        // Todo: 1 connect camera manager (which manages camera flashlight)
         CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
         try {
-            // ask if there is a camera, which has a flashlight.
-            for( String id: cameraManager.getCameraIdList() ) {
+            for(String id : cameraManager.getCameraIdList()){
                 CameraCharacteristics cameraChar = cameraManager.getCameraCharacteristics(id);
-
-                if(cameraChar.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)) {
-                    // set the flashlight of that camera on (torch mode to true)
-                    if(!torchOn) { cameraManager.setTorchMode(id, true); }
-                    else { cameraManager.setTorchMode(id, false); }
-                    torchOn = !torchOn;
+                if(cameraChar.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)){
+                    //Todo: 2 Ask if there is a camera which has a flashlight
+                    if(!torchon) {
+                        //todo: 3 set the flashlight of that camera  on ()torch modetrue or not
+                        cameraManager.setTorchMode(id, true);
+                        Toast.makeText(MainActivity.this, "Flashlight is on", Toast.LENGTH_SHORT).show();
+                    }else{
+                        cameraManager.setTorchMode(id, false);
+                        Toast.makeText(MainActivity.this, "Flash is off", Toast.LENGTH_SHORT).show();
+                    }torchon = !torchon;
                 }
+
             }
-        } catch(CameraAccessException e){
+        } catch (CameraAccessException e) {
             e.printStackTrace();
         }
 
     }
 
     public void startSensors(View view) {
+        //todo : start listening to acceleration sensor event (x,y,z position of the device)
+        //todo 1 : Connect sensor manager
         SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-//        for(Sensor s: sensorList ) {
+        LocationManager map = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Lists the number of sensor types available in the device using TOAST
+//        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+//        for(Sensor s: sensorList){
 //            Toast.makeText(this, s.getName(), Toast.LENGTH_SHORT).show();
 //        }
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        //todo 2 : register listen to acclerometer sensor events from teh HW
+        Sensor acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        //getting sensor events
+        // Read xyz values and show in textview
         float x = sensorEvent.values[0];
         float y = sensorEvent.values[1];
         float z = sensorEvent.values[2];
-
-        TextView sensorText = (TextView) findViewById(R.id.sensorTextView);
-        sensorText.setText("X: " + x + "Y: " + y + "Z: " + z);
+        TextView sensorTextView = findViewById(R.id.SensorTextView);
+        sensorTextView.setText("X: "+x+ "Y: "+y+ "Z: "+z);
+        ConstraintLayout background = findViewById(R.id.background);
+        if (z>-0.3 && z<0.3){
+            background.setBackgroundColor(Color.GREEN);
+        }else{
+            background.setBackgroundColor(Color.BLUE);
+        }
     }
 
     @Override
